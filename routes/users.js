ff2d9a3e37d884ba1bc601d238744c142e3870de
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 15:30:30 -0600

    following end point is working

--- routes/users.js
+++ routes/users.js
@@ -29,6 +29,7 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// delete user
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
@@ -42,6 +43,7 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+// get user
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
@@ -50,6 +52,27 @@ router.get("/:id", async (req, res) => {
     res.status(200).json(other);
   } catch (err) {
     return res.status(500).json(err);
+  }
+});
+
+// follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userIdd !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json("You already follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you can't follow yourself");
   }
 });
 
