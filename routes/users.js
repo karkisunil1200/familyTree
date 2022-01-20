affbaadda9469134e123f528ab6a9197b0ab972f
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 15:56:28 -0600

    Unfollow endpoint works

--- routes/users.js
+++ routes/users.js
@@ -57,7 +57,7 @@ router.get("/:id", async (req, res) => {
 
 // follow a user
 router.put("/:id/follow", async (req, res) => {
-  if (req.body.userIdd !== req.params.id) {
+  if (req.body.userId !== req.params.id) {
     try {
       const user = await User.findById(req.params.id);
       const currentUser = await User.findById(req.body.userId);
@@ -73,6 +73,28 @@ router.put("/:id/follow", async (req, res) => {
     }
   } else {
     res.status(403).json("you can't follow yourself");
+  }
+});
+
+//unfollow
+
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("user has been unfollowed");
+      } else {
+        res.status(403).json("you don't follow thiis user");
+      }
+    } catch (err) {
+      res.status(403).json(err);
+    }
+  } else {
+    res.status(403).json("you don't follow this user");
   }
 });
 
