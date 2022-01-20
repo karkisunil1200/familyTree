fca9b07cfb73bf01d5634f662e73fe8fe84c45ea
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 12:13:26 -0600

    Added bcrypt library

--- models/User.js
+++ models/User.js
@@ -1,42 +1,45 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: "true",
-    min: 3,
-    max: 30,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    max: 50,
-    uniquie: true,
-  },
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: "true",
+      min: 3,
+      max: 30,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      uniquie: true,
+    },
 
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-  },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
 
-  profilePicture: {
-    type: String,
-    default: "",
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  followers: {
-    type: Array,
-    default: [],
-  },
-  followings: {
-    type: Array,
-    default: [],
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
