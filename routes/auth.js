cb852a10e8c85cbe9b1fe8e06bc7f81b3beac214
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 13:40:09 -0600

    put working

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { route } = require("./users");
 
 router.post("/register", async (req, res) => {
   console.log("before...");
@@ -21,6 +22,26 @@ router.post("/register", async (req, res) => {
     res.status(200).json(user);
   } catch (err) {
     console.log(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    //compare the password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
