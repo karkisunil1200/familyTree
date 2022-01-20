852b0fa239e83f5f9c1e009281b3ce356e1488c4
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 14:19:53 -0600

    Added delete

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt");
 //update user
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
+    // changes password
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -13,6 +14,7 @@ router.put("/:id", async (req, res) => {
         return res.status(500).json(err);
       }
     }
+    // update everything else
     try {
       const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
@@ -23,6 +25,19 @@ router.put("/:id", async (req, res) => {
     }
   } else {
     return res.status(403).json("You can update only your account!");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findOneAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
   }
 });
 
