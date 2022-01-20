011ad8df365aa43e83aeb25af8c7e4852a63557c
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 06:40:15 -0600

    register barebones working

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
-router.get("/register", (req, res) => {
-  const user = new User({
+router.get("/register", async (req, res) => {
+  const user = await new User({
     username: "John",
     email: "john@gmail.com",
     password: "Nepal123",
   });
 
-  res.send("ok it is still good.");
+  user.save();
+  await res.send("ok it is Working!!!.");
 });
 
 module.exports = router;
