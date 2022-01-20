b44645c2777b175f1f7a9979c17d3e76b54949bf
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 06:17:05 -0600

    Added user data model

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const res = require("express/lib/response");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connected to Mongo");
 });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage!!");
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => console.log("Backend server is running..."));
