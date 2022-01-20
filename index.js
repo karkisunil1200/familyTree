0f801c00f7ff4a0750426e2fc445455f39cc6f17
sunilkarki <karkisunil1200@gmail.com>
2022-01-20 05:55:23 -0600

    Added connections to mongodb

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to Mongo");
+});
 
 app.listen(8800, () => console.log("Backend server is running..."));
