dce73f4fee740882a27ed7a7f78bcb4a0d54b8de
sunilkarki <karkisunil1200@gmail.com>
2022-01-21 10:08:18 -0600

    Delete endpoint working

--- routes/posts.js
+++ routes/posts.js
@@ -12,8 +12,35 @@ router.post("/", async (req, res) => {
   }
 });
 //update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can only update your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      console.log("deleting...");
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //likse a post
 
