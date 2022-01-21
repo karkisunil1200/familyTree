538d7fa6214a3e3a64e26a2c41c5e52faf8bc399
sunilkarki <karkisunil1200@gmail.com>
2022-01-21 13:46:10 -0600

    Added timeline posts

--- routes/posts.js
+++ routes/posts.js
@@ -71,7 +71,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.get("/timeline", async (req, res) => {
+router.get("/timeline/all", async (req, res) => {
   try {
     const currentUser = await User.findById(req.body.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
