953ad1a308a7fca686360615372c570cb37397fd
sunilkarki <karkisunil1200@gmail.com>
2022-01-21 13:42:01 -0600

    Added like and dislike

--- routes/posts.js
+++ routes/posts.js
@@ -46,11 +46,31 @@ router.delete("/:id", async (req, res) => {
 });
 
 // //likse a post
-// router.get("/:id/likes", (req, res) => {
-//   res.send("I am here!!");
-// });
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
 
 //get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
 router.get("/timeline", async (req, res) => {
   try {
     const currentUser = await User.findById(req.body.userId);
